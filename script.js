c5414c2be8c76a6a91ad412ea1f4e0abbe249678
Menglangdada <76909710+Menglangdada@users.noreply.github.com>
2025-10-05 23:51:49 -0400

    modified:   script.js Remove a row

--- script.js
+++ script.js
@@ -5,17 +5,51 @@ let colorSelected;
 
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+    let grid = document.getElementById("grid");
+    let row = grid.insertRow(numRows);
+    if (numCols === 0) {
+        let cell = row.insertCell(0);
+        cell.onclick = function(){
+            this.style.backgroundColor = colorSelected;
+        };
+        numCols++;
+    }
+    else{
+        for (let i = 0; i < numCols; i++) {
+            let cell = row.insertCell(i);
+            cell.onclick = function(){
+                this.style.backgroundColor = colorSelected;
+            };
+        }
+    }
+    numRows++;
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+    let rows = document.getElementsByTagName("tr");
+    if(rows.length === 0){
+        addR();
+        return;
+    }
+    for (let i = 0; i < numRows; i++) {
+        let cell = rows[i].insertCell(numCols);
+        cell.onclick = function(){
+            this.style.backgroundColor = colorSelected;
+        };
+    }
+    numCols++;
 }
 
 // Remove a row
 function removeR() {
-    alert("Clicked Remove Row"); // Replace this line with your code.
+    if(numRows === 0) return;
+    let grid = document.getElementById("grid");
+    grid.deleteRow(numRows-1);
+    numRows--;
+    if(numRows === 0){
+        numCols = 0;
+    }
 }
 
 // Remove a column
